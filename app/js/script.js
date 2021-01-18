630c9a94638ae7312d1e0f040a0a3ba81fa3e113
Vaibhav <Vaibhav97singh21@gmail.com>
2021-01-18 17:29:32 +0530

    output Resolved

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,23 @@
-console.log('Hello World');
+var inputTxt = document.querySelector("#inputText");
+var translateBtn = document.querySelector(".button");
+var outputMsg = document.querySelector(".outputText");
+
+var serverURL = "https://api.funtranslations.com/translate/mandalorian.json" ;
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text;
+}
+
+function errHandler(error) {
+    console.log("error occured, "+ error);
+    alert("Server is down!! try again after some time");
+}
+
+translateBtn.addEventListener("click", function clickHandler() {
+    var text = inputTxt.value; //taking input
+
+    fetch(getTranslationURL(text)).then(response => response.json()).then(json => {
+        outputMsg.innerText = (json.contents.translated)
+    }).catch(errHandler);
+    
+});
